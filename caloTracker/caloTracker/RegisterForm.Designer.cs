8c965416a77b0ea7cbd83eeac6afdfdf9ddfba39
Diego Morales <diego.morales@alumnos.uneatlantico.es>
2023-12-05 00:55:29 +0100

    Register/Login/Main form

---
+++ caloTracker/caloTracker/RegisterForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace caloTracker
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelGoToLogin = new System.Windows.Forms.Label();
+            this.textBoxPasswordConfirm = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.buttonCreateAccount = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelClose = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Controls.Add(this.labelGoToLogin);
+            this.panel1.Controls.Add(this.textBoxPasswordConfirm);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Controls.Add(this.textBoxEmail);
+            this.panel1.Controls.Add(this.textBoxLastname);
+            this.panel1.Controls.Add(this.buttonCreateAccount);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxFirstname);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(821, 654);
+            this.panel1.TabIndex = 1;
+            // 
+            // labelGoToLogin
+            // 
+            this.labelGoToLogin.AutoSize = true;
+            this.labelGoToLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelGoToLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGoToLogin.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelGoToLogin.Location = new System.Drawing.Point(305, 571);
+            this.labelGoToLogin.Name = "labelGoToLogin";
+            this.labelGoToLogin.Size = new System.Drawing.Size(199, 16);
+            this.labelGoToLogin.TabIndex = 201;
+            this.labelGoToLogin.Text = "Already have an Account? Login";
+            this.labelGoToLogin.Click += new System.EventHandler(this.labelGoToLogin_Click);
+            this.labelGoToLogin.MouseEnter += new System.EventHandler(this.labelGoToLogin_MouseEnter);
+            this.labelGoToLogin.MouseLeave += new System.EventHandler(this.labelGoToLogin_MouseLeave);
+            // 
+            // textBoxPasswordConfirm
+            // 
+            this.textBoxPasswordConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPasswordConfirm.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxPasswordConfirm.Location = new System.Drawing.Point(137, 421);
+            this.textBoxPasswordConfirm.Name = "textBoxPasswordConfirm";
+            this.textBoxPasswordConfirm.Size = new System.Drawing.Size(538, 49);
+            this.textBoxPasswordConfirm.TabIndex = 10;
+            this.textBoxPasswordConfirm.Text = "confirm password";
+            this.textBoxPasswordConfirm.Enter += new System.EventHandler(this.textBoxPasswordConfirm_Enter);
+            this.textBoxPasswordConfirm.Leave += new System.EventHandler(this.textBoxPasswordConfirm_Leave);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxUsername.Location = new System.Drawing.Point(137, 274);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(538, 49);
+            this.textBoxUsername.TabIndex = 9;
+            this.textBoxUsername.Text = "username";
+            this.textBoxUsername.Enter += new System.EventHandler(this.textBoxUsername_Enter);
+            this.textBoxUsername.Leave += new System.EventHandler(this.textBoxUsername_Leave);
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxEmail.Location = new System.Drawing.Point(137, 199);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(538, 49);
+            this.textBoxEmail.TabIndex = 8;
+            this.textBoxEmail.Text = "email address";
+            this.textBoxEmail.Enter += new System.EventHandler(this.textBoxEmail_Enter);
+            this.textBoxEmail.Leave += new System.EventHandler(this.textBoxEmail_Leave);
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLastname.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxLastname.Location = new System.Drawing.Point(412, 129);
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(260, 49);
+            this.textBoxLastname.TabIndex = 7;
+            this.textBoxLastname.Text = "last name";
+            this.textBoxLastname.Enter += new System.EventHandler(this.textBoxLastname_Enter);
+            this.textBoxLastname.Leave += new System.EventHandler(this.textBoxLastname_Leave);
+            // 
+            // buttonCreateAccount
+            // 
+            this.buttonCreateAccount.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.buttonCreateAccount.FlatAppearance.BorderSize = 0;
+            this.buttonCreateAccount.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCreateAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreateAccount.Location = new System.Drawing.Point(137, 494);
+            this.buttonCreateAccount.Name = "buttonCreateAccount";
+            this.buttonCreateAccount.Size = new System.Drawing.Size(538, 60);
+            this.buttonCreateAccount.TabIndex = 6;
+            this.buttonCreateAccount.Text = "CREATE ACCOUNT";
+            this.buttonCreateAccount.UseVisualStyleBackColor = false;
+            this.buttonCreateAccount.Click += new System.EventHandler(this.buttonCreateAccount_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxPassword.Location = new System.Drawing.Point(137, 347);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(538, 49);
+            this.textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.Text = "password";
+            this.textBoxPassword.Enter += new System.EventHandler(this.textBoxPassword_Enter);
+            this.textBoxPassword.Leave += new System.EventHandler(this.textBoxPassword_Leave);
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFirstname.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.textBoxFirstname.Location = new System.Drawing.Point(137, 129);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(260, 49);
+            this.textBoxFirstname.TabIndex = 200;
+            this.textBoxFirstname.Text = "first name";
+            this.textBoxFirstname.Enter += new System.EventHandler(this.textBoxFirstname_Enter);
+            this.textBoxFirstname.Leave += new System.EventHandler(this.textBoxFirstname_Leave);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel2.Controls.Add(this.labelClose);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(822, 103);
+            this.panel2.TabIndex = 0;
+            // 
+            // labelClose
+            // 
+            this.labelClose.AutoSize = true;
+            this.labelClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelClose.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelClose.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.labelClose.Location = new System.Drawing.Point(789, 0);
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Size = new System.Drawing.Size(30, 29);
+            this.labelClose.TabIndex = 1;
+            this.labelClose.Text = "X";
+            this.labelClose.Click += new System.EventHandler(this.labelClose_Click);
+            this.labelClose.MouseEnter += new System.EventHandler(this.labelClose_MouseEnter);
+            this.labelClose.MouseLeave += new System.EventHandler(this.labelClose_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(822, 103);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CREATE YOUR ACCOUNT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 654);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RegisterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RegisterForm";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonCreateAccount;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPasswordConfirm;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.Label labelGoToLogin;
+    }
+}
---
+++ caloTracker/caloTracker/RegisterForm.cs
@@ -0,0 +1,298 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace caloTracker
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = label1;
+        }
+
+        private void textBoxFirstname_Enter(object sender, EventArgs e)
+        {
+            String fname = textBoxFirstname.Text;
+            if(fname.ToLower().Trim().Equals("first name"))
+            {
+                textBoxFirstname.Text = "";
+                textBoxFirstname.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxFirstname_Leave(object sender, EventArgs e)
+        {
+            String fname = textBoxFirstname.Text;
+            if(fname.ToLower().Trim().Equals("first name") || fname.Trim().Equals(""))
+            {
+                textBoxFirstname.Text = "first name";
+                textBoxFirstname.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxLastname_Enter(object sender, EventArgs e)
+        {
+            String lname = textBoxLastname.Text;
+            if (lname.ToLower().Trim().Equals("last name"))
+            {
+                textBoxLastname.Text = "";
+                textBoxLastname.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxLastname_Leave(object sender, EventArgs e)
+        {
+            String lname = textBoxLastname.Text;
+            if (lname.ToLower().Trim().Equals("last name") || lname.Trim().Equals(""))
+            {
+                textBoxLastname.Text = "last name";
+                textBoxLastname.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxEmail_Enter(object sender, EventArgs e)
+        {
+            String email = textBoxEmail.Text;
+            if (email.ToLower().Trim().Equals("email address"))
+            {
+                textBoxEmail.Text = "";
+                textBoxEmail.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxEmail_Leave(object sender, EventArgs e)
+        {
+            String email = textBoxEmail.Text;
+            if (email.ToLower().Trim().Equals("email address") || email.Trim().Equals(""))
+            {
+                textBoxEmail.Text = "email address";
+                textBoxEmail.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxUsername_Enter(object sender, EventArgs e)
+        {
+            String user = textBoxUsername.Text;
+            if (user.ToLower().Trim().Equals("username"))
+            {
+                textBoxUsername.Text = "";
+                textBoxUsername.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxUsername_Leave(object sender, EventArgs e)
+        {
+            String user = textBoxUsername.Text;
+            if (user.ToLower().Trim().Equals("username") || user.Trim().Equals(""))
+            {
+                textBoxUsername.Text = "username";
+                textBoxUsername.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxPassword_Enter(object sender, EventArgs e)
+        {
+            String password = textBoxPassword.Text;
+            if (password.ToLower().Trim().Equals("password"))
+            {
+                textBoxPassword.Text = "";
+                textBoxPassword.UseSystemPasswordChar = true;
+                textBoxPassword.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxPassword_Leave(object sender, EventArgs e)
+        {
+            String password = textBoxPassword.Text;
+            if (password.ToLower().Trim().Equals("password") || password.Trim().Equals(""))
+            {
+                textBoxPassword.Text = "password";
+                textBoxPassword.UseSystemPasswordChar = false;
+                textBoxPassword.ForeColor = Color.Gray;
+            }
+        }
+
+        private void textBoxPasswordConfirm_Enter(object sender, EventArgs e)
+        {
+            String cpassword = textBoxPasswordConfirm.Text;
+            if (cpassword.ToLower().Trim().Equals("confirm password"))
+            {
+                textBoxPasswordConfirm.Text = "";
+                textBoxPasswordConfirm.UseSystemPasswordChar = true;
+                textBoxPasswordConfirm.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBoxPasswordConfirm_Leave(object sender, EventArgs e)
+        {
+            String cpassword = textBoxPasswordConfirm.Text;
+            if (cpassword.ToLower().Trim().Equals("confirm password") || cpassword.Trim().Equals(""))
+            {
+                textBoxPasswordConfirm.Text = "confirm password";
+                textBoxPasswordConfirm.UseSystemPasswordChar = false;
+                textBoxPasswordConfirm.ForeColor = Color.Gray;
+            }
+        }
+
+        private void labelClose_Click(object sender, EventArgs e)
+        {
+            //this.Close();
+            Application.Exit();
+        }
+
+        private void labelClose_Enter(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.Black;
+        }
+
+        private void labelClose_Leave(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.White;
+        }
+
+        private void labelClose_MouseEnter(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.Black;
+
+        }
+
+        private void labelClose_MouseLeave(object sender, EventArgs e)
+        {
+            labelClose.ForeColor = Color.White;
+        }
+
+        private void buttonCreateAccount_Click(object sender, EventArgs e)
+        {
+            // add a new user 
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users`(`firstname`, `lastname`, `email`, `username`, `password`) VALUES (@fn, @ln, @email, @usn, @pass)", db.getConnection());
+
+            command.Parameters.Add("@fn", MySqlDbType.VarChar).Value = textBoxFirstname.Text;
+            command.Parameters.Add("@ln", MySqlDbType.VarChar).Value = textBoxLastname.Text;
+            command.Parameters.Add("@email", MySqlDbType.VarChar).Value = textBoxEmail.Text;
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = textBoxUsername.Text;
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = textBoxPassword.Text;
+
+            // open the connection 
+            db.OpenConnection();
+
+
+            // Checks if textboxes contains default value
+            if (!checkTextBoxesValues())
+            {
+
+                // Check if the password equal the confirm password
+                if (textBoxPassword.Text.Equals(textBoxPasswordConfirm.Text))
+                {
+                    // Checks if username already exists
+                    if (checkUsername())
+                    {
+                        MessageBox.Show("This username already exists!!","Duplicate Username", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+
+                    }
+                    else
+                    {
+
+                        // execute the query
+                        if (command.ExecuteNonQuery() == 1)
+                        {
+                            MessageBox.Show("YOUR ACCOUNT HAS BEEN CREATED", "ACCOUNT CREATED", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            this.Hide();
+                            LoginForm loginForm = new LoginForm();
+                            loginForm.Show();
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("ERROR");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("PASSWORD DOESN'T MATCH" ,"PASSWORD ERROR", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("ENTER YOUR INFORMATION!!", "EMPTY DATA", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+
+        //check if the user already exists
+        public Boolean checkUsername()
+        {
+            DB db = new DB();
+
+            String username = textBoxUsername.Text;
+
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `username` = @usn", db.getConnection());
+            command.Parameters.Add("@usn", MySqlDbType.VarChar).Value = username;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            //Check if the username exists
+            if (table.Rows.Count > 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Check if textbox contains default values
+        public Boolean checkTextBoxesValues()
+        {
+            String fname = textBoxFirstname.Text;
+            String lname = textBoxLastname.Text;
+            String username = textBoxUsername.Text;
+            String email = textBoxEmail.Text;
+            String password = textBoxPassword.Text;
+
+            if(fname.Equals("first name") || lname.Equals("last name") || username.Equals("username") || email.Equals("email") || password.Equals("password")) 
+            { 
+                return true; 
+            } else
+            {
+                return false;
+            }
+        }
+
+        private void labelGoToLogin_MouseEnter(object sender, EventArgs e)
+        {
+            labelGoToLogin.ForeColor = Color.Yellow;
+        }
+
+        private void labelGoToLogin_MouseLeave(object sender, EventArgs e)
+        {
+            labelGoToLogin.ForeColor = Color.White;
+        }
+
+        private void labelGoToLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+    }
+}
